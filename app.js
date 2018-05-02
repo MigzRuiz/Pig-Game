856203bf9733752c1742449755c7d9bbe80b185d
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-05-01 20:39:47 -0700

    Added the state and the new game functions

--- app.js
+++ app.js
@@ -9,19 +9,10 @@ GAME RULES:
 
 */
 //VARIABLES
-var score = [0,0];
-var roundScore = 0;
-var activePlayer = 0;
+var score, roundScore, activePlayer, gamePlaying;
 
-//getElementById is faster than querySelector
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
 
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0"
-
-//To manipulate CSS = use .style.<then the style>
-document.querySelector(".dice").style.display = "none";
+init();
 
 //EVENTS AND EVENT HANDLING
     /*The first way is doing a callback function = calling a function that was passed as an argument on another function.
@@ -33,53 +24,53 @@ document.querySelector(".dice").style.display = "none";
 
     //Second way is writing an annonymous function like this:
 document.querySelector(".btn-roll").addEventListener("click", function() {
-    /*We want to do something each time the btn-roll class is clicked.
-        1.) We want a random number from 1 to 6;
-        2.) We want to display that random number;
-        3.) We want to update the score but ONLY IF the number is NOT 1;
-    */
-//1
-    var dice = Math.floor(Math.random() * 6) + 1;
-//2
-    var diceDOM = document.querySelector(".dice");
-    diceDOM.style.display = "unset";
-    diceDOM.src="dice-" + dice + ".png";
-//3
-    if(dice > 1) {
-        //Add to score
-        roundScore += dice;
-        document.getElementById("current-" + activePlayer).textContent = roundScore;
-    } else {
-        //Next player
-        nextPlayer();
-        //document.querySelector(".player-0-panel").classList.add('active');
-        //document.querySelector(".player-1-panel").classList.remove('active');
+    if (gamePlaying) {
+        //1 We want a random number from 1 to 6;
+        var dice = Math.floor(Math.random() * 6) + 1;
+        //2 We want to display that random number;
+        var diceDOM = document.querySelector(".dice");
+        diceDOM.style.display = "unset";
+        diceDOM.src="dice-" + dice + ".png";
+        //3 We want to update the score but ONLY IF the number is NOT 1;
+        if(dice > 1) {
+            //Add to score
+            roundScore += dice;
+            document.getElementById("current-" + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            nextPlayer();
+            //document.querySelector(".player-0-panel").classList.add('active');
+            //document.querySelector(".player-1-panel").classList.remove('active');
+        }
     }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function(){
-    //Variables
-    var diceDOM = document.querySelector(".dice");
+    if(gamePlaying) {
+        //Variables
+        var diceDOM = document.querySelector(".dice");
 
-    //Transfer the roundScore to the actual score
-    score[activePlayer] += roundScore;
+        //Transfer the roundScore to the actual score
+        score[activePlayer] += roundScore;
 
-    //Show that in the DOM
-    document.getElementById("score-" + activePlayer).textContent = score[activePlayer];
+        //Show that in the DOM
+        document.getElementById("score-" + activePlayer).textContent = score[activePlayer];
 
-    //Calculate if player won
-    if(score[activePlayer] >= 20) {
-        document.getElementById("name-" + activePlayer).textContent = "Winner!";
-        document.querySelector(".dice").style.display = "none";
-        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
-        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
-    } else {
-        //Switch to next player
-        nextPlayer();
+        //Calculate if player won
+        if(score[activePlayer] >= 20) {
+            document.getElementById("name-" + activePlayer).textContent = "Winner!";
+            document.querySelector(".dice").style.display = "none";
+            document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+            document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+            gamePlaying = false;
+        } else {
+            //Switch to next player
+            nextPlayer();
+        }
     }
-
-
 });
+
+document.querySelector(".btn-new").addEventListener("click", init);
 
 function nextPlayer() {
     var diceDOM = document.querySelector(".dice");
@@ -91,6 +82,34 @@ function nextPlayer() {
 
     diceDOM.style.display = "none";
     roundScore = 0;
+}
+
+function init(){
+    score = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+    WgamePlaying = true;
+
+    //getElementById is faster than querySelector
+    document.getElementById("score-0").textContent = "0";
+    document.getElementById("score-1").textContent = "0";
+
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0"
+
+    document.getElementById("name-0").textContent = "Player 1";
+    document.getElementById("name-1").textContent = "Player 2";
+
+
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+    
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.remove("active");
+
+    document.querySelector(".player-0-panel").classList.add("active");
+    //To manipulate CSS = use .style.<then the style>
+    document.querySelector(".dice").style.display = "none";
 
 }
 
