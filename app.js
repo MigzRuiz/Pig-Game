98923fa9d3c0e80204083a91a1e06da162a63f9e
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-05-01 20:44:34 -0700

    Initial settings for the challenges

--- app.js
+++ app.js
@@ -8,46 +8,32 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-//VARIABLES
 var score, roundScore, activePlayer, gamePlaying;
-
 
 init();
 
-//EVENTS AND EVENT HANDLING
-    /*The first way is doing a callback function = calling a function that was passed as an argument on another function.
-        function btn(){
-            //Do something here
-        }
-        document.querySelector(".btn-roll").addEventListener("click", btn);
-    */
-
-    //Second way is writing an annonymous function like this:
 document.querySelector(".btn-roll").addEventListener("click", function() {
     if (gamePlaying) {
-        //1 We want a random number from 1 to 6;
+        //Get a random number from 1 to 6;
         var dice = Math.floor(Math.random() * 6) + 1;
-        //2 We want to display that random number;
+
+        //Display that random number;
         var diceDOM = document.querySelector(".dice");
         diceDOM.style.display = "unset";
         diceDOM.src="dice-" + dice + ".png";
-        //3 We want to update the score but ONLY IF the number is NOT 1;
+
+        //Update the score but ONLY IF the number is NOT 1;
         if(dice > 1) {
-            //Add to score
             roundScore += dice;
             document.getElementById("current-" + activePlayer).textContent = roundScore;
         } else {
-            //Next player
             nextPlayer();
-            //document.querySelector(".player-0-panel").classList.add('active');
-            //document.querySelector(".player-1-panel").classList.remove('active');
         }
     }
 });
 
 document.querySelector(".btn-hold").addEventListener("click", function(){
     if(gamePlaying) {
-        //Variables
         var diceDOM = document.querySelector(".dice");
 
         //Transfer the roundScore to the actual score
@@ -64,7 +50,6 @@ document.querySelector(".btn-hold").addEventListener("click", function(){
             document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
             gamePlaying = false;
         } else {
-            //Switch to next player
             nextPlayer();
         }
     }
@@ -108,17 +93,7 @@ function init(){
     document.querySelector(".player-1-panel").classList.remove("active");
 
     document.querySelector(".player-0-panel").classList.add("active");
-    //To manipulate CSS = use .style.<then the style>
+
     document.querySelector(".dice").style.display = "none";
 
 }
-
-//var dice = Math.floor(Math.random() * 6) + 1;
-
-//For us to be able to set a value in the DOM
-//document.querySelector("#current-" + activePlayer).textContent = dice;
-//document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
-
-//To get value in the DOM
-//var x = document.querySelector("#score-0").innerHTML;
-//console.log(x);
